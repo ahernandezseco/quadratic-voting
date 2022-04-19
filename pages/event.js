1f36052b90336b6b2908b8a212a866c94af91170
Kirk <a.hernandez.seco@gmail.com>
2022-04-19 23:33:21 +0200

    fix url

--- pages/event.js
+++ pages/event.js
@@ -59,7 +59,7 @@ function Event({ query }) {
   const downloadTXT = () => {
     // Collect voter URLs in single text string
     const text = data.event.voters
-      .map((voter, _) => `https://quadraticvote.co/vote?user=${voter.id}`)
+      .map((voter, _) => `https://cbre-quadratic-voting.netlify.app/vote?user=${voter.id}`)
       .join("\n");
 
     // Create link component
@@ -123,7 +123,7 @@ function Event({ query }) {
           <label>Event URL</label>
           <p>Statistics dashboard URL</p>
           <input
-            value={`https://quadraticvote.co/event?id=${query.id}`}
+            value={`https://cbre-quadratic-voting.netlify.app/event?id=${query.id}`}
             readOnly
           />
         </div>
@@ -139,7 +139,7 @@ function Event({ query }) {
               <label className="private__label">Private Admin URL</label>
               <p>Save this URL to manage event and make changes</p>
               <input
-                value={`https://quadraticvote.co/event?id=${query.id}&secret=${query.secret}`}
+                value={`https://cbre-quadratic-voting.netlify.app/event?id=${query.id}&secret=${query.secret}`}
                 readOnly
               />
             </div>
@@ -170,7 +170,7 @@ function Event({ query }) {
               // Collect voter urls as one text element
               value={data.event.voters
                 .map(
-                  (voter, _) => `https://quadraticvote.co/vote?user=${voter.id}`
+                  (voter, _) => `https://cbre-quadratic-voting.netlify.app/vote?user=${voter.id}`
                 )
                 .join("\n")}
               readOnly
